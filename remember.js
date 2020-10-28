aeb04218a75d817057354b3dfac79f7f5f768bcb
Alexander <alexanderespinozajesus@gmail.com>
2020-10-28 00:43:05 -0500

    Ingresé un cambio
    Ingresé un console.log

--- remember.js
+++ remember.js
@@ -2,4 +2,5 @@ alert("Trying to learn GitHub. It's interesting");
 //POO
 console.log("Remembering any memories");
 //Clase -- Idea 
+console.log("Cambio");
 //Objeto --Construido a partir de la clase. Instancia de clase
