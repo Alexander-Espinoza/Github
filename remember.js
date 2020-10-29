4d3c523f1f86ae0529ec5090f99c7ed80031a222
Alexander Espinoza <68801790+ElAlisR@users.noreply.github.com>
2020-10-29 16:54:26 -0500

    Cambio desde remoto

--- remember.js
+++ remember.js
@@ -1,7 +1,10 @@
-﻿alert("Trying to learn GitHub. It's interesting");
+alert("Trying to learn GitHub. It's interesting");
 //POO
 console.log("Remembering any memories");
 //Clase -- Idea 
 console.log("Cambio");
 //Objeto --Construido a partir de la clase. Instancia de clase
+
+//Actualizado desde remoto
+console.log("Cambio desde remoto");
 
