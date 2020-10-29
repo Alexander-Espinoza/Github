270ef5e4b93ad36f0f650ee71e48fd105cb20623
Alexander <alexanderespinozajesus@gmail.com>
2020-10-29 16:53:08 -0500

    Actualizado
    Actualizado desde remoto

--- remember.js
+++ remember.js
@@ -1,7 +1,7 @@
-alert("Trying to learn GitHub. It's interesting");
+﻿alert("Trying to learn GitHub. It's interesting");
 //POO
 console.log("Remembering any memories");
 //Clase -- Idea 
 console.log("Cambio");
 //Objeto --Construido a partir de la clase. Instancia de clase
-console.log("Cambiado en línea");
+
